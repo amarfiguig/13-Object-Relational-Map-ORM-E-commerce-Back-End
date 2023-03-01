bf3c84142f4ec2594341505c204a593744cbfa79
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-01 11:57:00 +0000

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,21 +1,16 @@
-// import models
+// Import necessary models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Define relationships between models using Sequelize associations
+Product.belongsTo(Category, { foreignKey: 'category_id' }); // Each product belongs to one category
+Category.hasMany(Product, { foreignKey: 'category_id' }); // Each category can have many products
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' }); // Each product can have many tags, and each tag can be applied to many products through ProductTag
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-Product.belongsTo(Category, {foreignKey: 'category_id'});
-Category.hasMany(Product, {foreignKey: 'product_id'});
-Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id'});
-Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id'});
-
+// Export all models for use in other parts of the application
 module.exports = {
   Product,
   Category,
